ee6eecf96897e12318c093e8f348bf512463421c
saide <saide@DESKTOP-T9H6SOU>
2022-02-10 22:22:31 +0200

    Add project files.

---
+++ ThePlatformPlatformerGame/Views/LevelSelect.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ThePlatformPlatformerGame
+{
+    partial class LevelSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.btnLvl1 = new System.Windows.Forms.Button();
+            this.btnLvl2 = new System.Windows.Forms.Button();
+            this.btnLvl3 = new System.Windows.Forms.Button();
+            this.btnLvl6 = new System.Windows.Forms.Button();
+            this.btnLvl5 = new System.Windows.Forms.Button();
+            this.btnLvl4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.BackColor = System.Drawing.Color.Transparent;
+            this.btnUndo.FlatAppearance.BorderSize = 0;
+            this.btnUndo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUndo.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.btnUndo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnUndo.Location = new System.Drawing.Point(0, -2);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(68, 48);
+            this.btnUndo.TabIndex = 0;
+            this.btnUndo.Text = "⎌";
+            this.btnUndo.UseVisualStyleBackColor = false;
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.BackColor = System.Drawing.Color.Transparent;
+            this.lblSelect.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.lblSelect.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSelect.Location = new System.Drawing.Point(213, 23);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(327, 49);
+            this.lblSelect.TabIndex = 1;
+            this.lblSelect.Text = "Select Level";
+            // 
+            // btnLvl1
+            // 
+            this.btnLvl1.BackColor = System.Drawing.Color.Transparent;
+            this.btnLvl1.FlatAppearance.BorderSize = 0;
+            this.btnLvl1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLvl1.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.btnLvl1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLvl1.Location = new System.Drawing.Point(112, 175);
+            this.btnLvl1.Name = "btnLvl1";
+            this.btnLvl1.Size = new System.Drawing.Size(110, 91);
+            this.btnLvl1.TabIndex = 2;
+            this.btnLvl1.Text = "1";
+            this.btnLvl1.UseVisualStyleBackColor = false;
+            // 
+            // btnLvl2
+            // 
+            this.btnLvl2.BackColor = System.Drawing.Color.Transparent;
+            this.btnLvl2.FlatAppearance.BorderSize = 0;
+            this.btnLvl2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLvl2.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.btnLvl2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLvl2.Location = new System.Drawing.Point(328, 175);
+            this.btnLvl2.Name = "btnLvl2";
+            this.btnLvl2.Size = new System.Drawing.Size(110, 91);
+            this.btnLvl2.TabIndex = 3;
+            this.btnLvl2.Text = "2";
+            this.btnLvl2.UseVisualStyleBackColor = false;
+            // 
+            // btnLvl3
+            // 
+            this.btnLvl3.BackColor = System.Drawing.Color.Transparent;
+            this.btnLvl3.FlatAppearance.BorderSize = 0;
+            this.btnLvl3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLvl3.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.btnLvl3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLvl3.Location = new System.Drawing.Point(514, 175);
+            this.btnLvl3.Name = "btnLvl3";
+            this.btnLvl3.Size = new System.Drawing.Size(110, 91);
+            this.btnLvl3.TabIndex = 4;
+            this.btnLvl3.Text = "3";
+            this.btnLvl3.UseVisualStyleBackColor = false;
+            // 
+            // btnLvl6
+            // 
+            this.btnLvl6.BackColor = System.Drawing.Color.Transparent;
+            this.btnLvl6.FlatAppearance.BorderSize = 0;
+            this.btnLvl6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLvl6.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.btnLvl6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLvl6.Location = new System.Drawing.Point(514, 290);
+            this.btnLvl6.Name = "btnLvl6";
+            this.btnLvl6.Size = new System.Drawing.Size(110, 91);
+            this.btnLvl6.TabIndex = 7;
+            this.btnLvl6.Text = "6";
+            this.btnLvl6.UseVisualStyleBackColor = false;
+            // 
+            // btnLvl5
+            // 
+            this.btnLvl5.BackColor = System.Drawing.Color.Transparent;
+            this.btnLvl5.FlatAppearance.BorderSize = 0;
+            this.btnLvl5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLvl5.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.btnLvl5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLvl5.Location = new System.Drawing.Point(328, 290);
+            this.btnLvl5.Name = "btnLvl5";
+            this.btnLvl5.Size = new System.Drawing.Size(110, 91);
+            this.btnLvl5.TabIndex = 5;
+            this.btnLvl5.Text = "5";
+            this.btnLvl5.UseVisualStyleBackColor = false;
+            // 
+            // btnLvl4
+            // 
+            this.btnLvl4.BackColor = System.Drawing.Color.Transparent;
+            this.btnLvl4.FlatAppearance.BorderSize = 0;
+            this.btnLvl4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLvl4.Font = new System.Drawing.Font("Algerian", 25.8F, System.Drawing.FontStyle.Bold);
+            this.btnLvl4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLvl4.Location = new System.Drawing.Point(112, 290);
+            this.btnLvl4.Name = "btnLvl4";
+            this.btnLvl4.Size = new System.Drawing.Size(110, 91);
+            this.btnLvl4.TabIndex = 5;
+            this.btnLvl4.Text = "4";
+            this.btnLvl4.UseVisualStyleBackColor = false;
+            // 
+            // LevelSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ThePlatformPlatformerGame.Properties.Resources.example;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLvl6);
+            this.Controls.Add(this.btnLvl5);
+            this.Controls.Add(this.btnLvl4);
+            this.Controls.Add(this.btnLvl3);
+            this.Controls.Add(this.btnLvl2);
+            this.Controls.Add(this.btnLvl1);
+            this.Controls.Add(this.lblSelect);
+            this.Controls.Add(this.btnUndo);
+            this.DoubleBuffered = true;
+            this.Name = "LevelSelect";
+            this.Text = "Level Select";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.Button btnLvl1;
+        private System.Windows.Forms.Button btnLvl2;
+        private System.Windows.Forms.Button btnLvl3;
+        private System.Windows.Forms.Button btnLvl6;
+        private System.Windows.Forms.Button btnLvl5;
+        private System.Windows.Forms.Button btnLvl4;
+    }
+}
---
+++ ThePlatformPlatformerGame/Views/MainMenu.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ThePlatformPlatformerGame
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblPlatformer = new System.Windows.Forms.Label();
+            this.lblGame = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
+            this.btnPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.White;
+            this.btnPlay.Location = new System.Drawing.Point(304, 253);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(146, 64);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            // 
+            // lblPlatformer
+            // 
+            this.lblPlatformer.AutoSize = true;
+            this.lblPlatformer.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlatformer.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.8F, System.Drawing.FontStyle.Bold);
+            this.lblPlatformer.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPlatformer.Location = new System.Drawing.Point(172, 76);
+            this.lblPlatformer.Name = "lblPlatformer";
+            this.lblPlatformer.Size = new System.Drawing.Size(316, 69);
+            this.lblPlatformer.TabIndex = 1;
+            this.lblPlatformer.Text = "Platformer";
+            // 
+            // lblGame
+            // 
+            this.lblGame.AutoSize = true;
+            this.lblGame.BackColor = System.Drawing.Color.Transparent;
+            this.lblGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F, System.Drawing.FontStyle.Bold);
+            this.lblGame.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblGame.Location = new System.Drawing.Point(415, 130);
+            this.lblGame.Name = "lblGame";
+            this.lblGame.Size = new System.Drawing.Size(134, 31);
+            this.lblGame.TabIndex = 2;
+            this.lblGame.Text = "the game";
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ThePlatformPlatformerGame.Properties.Resources.background_0;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblGame);
+            this.Controls.Add(this.lblPlatformer);
+            this.Controls.Add(this.btnPlay);
+            this.DoubleBuffered = true;
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblPlatformer;
+        private System.Windows.Forms.Label lblGame;
+    }
+}
+
