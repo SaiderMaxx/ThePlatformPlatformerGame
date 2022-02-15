3e965ad50dff0a96c5d7488db758767a318e015c
Георги Дандиев <gd54376432@edu.mon.bg>
2022-02-16 00:19:23 +0200

    Done with the third level still ignoring the jump problem

--- ThePlatformPlatformerGame/Properties/Resources.Designer.cs
+++ ThePlatformPlatformerGame/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace ThePlatformPlatformerGame.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap coin {
+            get {
+                object obj = ResourceManager.GetObject("coin", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap door_closed {
             get {
                 object obj = ResourceManager.GetObject("door-closed", resourceCulture);
